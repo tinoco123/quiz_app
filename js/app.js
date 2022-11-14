5368e48b7780e3a5ffe4bfc5be5fc38224061952
unknown <UV@DGTI-COA838.coatza.intra.uv.mx>
2022-11-14 11:28:19 -0600

    Cuestionario web

---
+++ js/app.js
@@ -0,0 +1,4 @@
+import { colores } from "./prueba"; 
+alert("Bienvenido a la aplicación de cuestionarios")
+
+console.log(colores[0])
---
+++ js/prueba.js
@@ -0,0 +1,15 @@
+const colores = [
+
+
+    {
+        "rojo":"#f00",
+        "verde":"#0f0",
+        "azul":"#00f",
+        "cyan":"#0ff",
+        "magenta":"#f0f",
+        "amarillo":"#ff0",
+        "negro":"#000"
+    }
+    
+
+]    
